cae23d70d3a347da07a5f45d9f0c1064464bbb07
cf12 <11359669+cf12@users.noreply.github.com>
2025-05-16 19:25:53 -0400

    add opcodes type & error handling

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,5 +1,22 @@
 use core::fmt;
+use std::error::Error;
+use std::result::Result;
 use std::{borrow::BorrowMut, fs};
+
+#[derive(Debug)]
+pub enum Chip8Error {
+    InvalidInstruction(u16),
+}
+
+impl fmt::Display for Chip8Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::InvalidInstruction(op) => write!(f, "Invalid instruction: {:#04X}", op),
+        }
+    }
+}
+
+impl Error for Chip8Error {}
 
 pub const VIDEO_WIDTH: usize = 64;
 pub const VIDEO_HEIGHT: usize = 32;
@@ -45,6 +62,45 @@ pub struct Chip8 {
     st: u8,
 
     rng: fn() -> u8,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Opcode {
+    ClearScreen,
+    Return,
+    Jump(u16),
+    Call(u16),
+    SkipEqualByte(usize, u8),
+    SkipNotEqualByte(usize, u8),
+    SkipEqual(usize, usize),
+    LoadByte(usize, u8),
+    AddByte(usize, u8),
+    Load(usize, usize),
+    Add(usize, usize),
+    Or(usize, usize),
+    And(usize, usize),
+    Xor(usize, usize),
+    Sub(usize, usize),
+    SubN(usize, usize),
+    ShiftRight(usize),
+    ShiftLeft(usize),
+    SkipNotEqual(usize, usize),
+    LoadI(u16),
+    JumpV0(u16),
+    Random(usize, u8),
+    Draw(usize, usize, u8),
+    SkipKeyPress(usize),
+    SkipKeyNotPress(usize),
+    LoadDelayTimer(usize),
+    LoadKeyPress(usize),
+    LoadDelayTimerSet(usize),
+    LoadSoundTimer(usize),
+    AddI(usize),
+    LoadFont(usize),
+    LoadBCD(usize),
+    StoreRegisters(usize),
+    LoadRegisters(usize),
+    Invalid(u16),
 }
 
 impl fmt::Display for Chip8 {
@@ -99,171 +155,211 @@ impl Chip8 {
         self.keypad[key] = value;
     }
 
-    pub fn cycle(&mut self) {
-        // println!("{}", &self);
+    fn decode_opcode(&self, op: u16) -> Opcode {
+        let b1 = (op & 0xF000) >> 12;
+        let x = ((op & 0x0F00) >> 8) as usize;
+        let y = ((op & 0x00F0) >> 4) as usize;
+        let nnn = op & 0x0FFF;
+        let nn = (op & 0x00FF) as u8;
+        let n = (op & 0x000F) as u8;
+
+        match b1 {
+            0x0 => match nnn {
+                0x00E0 => Opcode::ClearScreen,
+                0x00EE => Opcode::Return,
+                _ => Opcode::Invalid(op),
+            },
+            0x1 => Opcode::Jump(nnn),
+            0x2 => Opcode::Call(nnn),
+            0x3 => Opcode::SkipEqualByte(x, nn),
+            0x4 => Opcode::SkipNotEqualByte(x, nn),
+            0x5 => Opcode::SkipEqual(x, y),
+            0x6 => Opcode::LoadByte(x, nn),
+            0x7 => Opcode::AddByte(x, nn),
+            0x8 => match n {
+                0x0 => Opcode::Load(x, y),
+                0x1 => Opcode::Or(x, y),
+                0x2 => Opcode::And(x, y),
+                0x3 => Opcode::Xor(x, y),
+                0x4 => Opcode::Add(x, y),
+                0x5 => Opcode::Sub(x, y),
+                0x6 => Opcode::ShiftRight(x),
+                0x7 => Opcode::SubN(x, y),
+                0xE => Opcode::ShiftLeft(x),
+                _ => Opcode::Invalid(op),
+            },
+            0x9 => Opcode::SkipNotEqual(x, y),
+            0xA => Opcode::LoadI(nnn),
+            0xB => Opcode::JumpV0(nnn),
+            0xC => Opcode::Random(x, nn),
+            0xD => Opcode::Draw(x, y, n),
+            0xE => match nn {
+                0x9E => Opcode::SkipKeyPress(x),
+                0xA1 => Opcode::SkipKeyNotPress(x),
+                _ => Opcode::Invalid(op),
+            },
+            0xF => match nn {
+                0x07 => Opcode::LoadDelayTimer(x),
+                0x0A => Opcode::LoadKeyPress(x),
+                0x15 => Opcode::LoadDelayTimerSet(x),
+                0x18 => Opcode::LoadSoundTimer(x),
+                0x1E => Opcode::AddI(x),
+                0x29 => Opcode::LoadFont(x),
+                0x33 => Opcode::LoadBCD(x),
+                0x55 => Opcode::StoreRegisters(x),
+                0x65 => Opcode::LoadRegisters(x),
+                _ => Opcode::Invalid(op),
+            },
+            _ => Opcode::Invalid(op),
+        }
+    }
+
+    fn fetch_opcode(&self) -> u16 {
         let op =
             ((self.mem[self.pc as usize] as u16) << 8) | (self.mem[(self.pc + 1) as usize] as u16);
+        op
+    }
+
+    pub fn cycle(&mut self) -> Result<(), Chip8Error> {
+        let raw_opcode = self.fetch_opcode();
+        let opcode = self.decode_opcode(raw_opcode);
 
         self.pc += 2;
 
-        let b1 = (op & 0xF000) >> 12;
-        #[allow(non_snake_case)]
-        let Vx = ((op & 0x0F00) >> 8) as usize;
-        #[allow(non_snake_case)]
-        let Vy = ((op & 0x00F0) >> 4) as usize;
-        let addr = op & 0x0FFF;
-        let byte = (op & 0x00FF) as u8;
-        let n = op & 0x000F;
+        match opcode {
+            // 00E0 - CLS
+            Opcode::ClearScreen => {
+                self.video.fill(false);
+            }
 
-        match b1 {
-            0x0 => {
-                match addr {
-                    // 00E0 - CLS
-                    0x0E0 => {
-                        self.video.fill(false);
-                    }
-
-                    // 00EE - RET
-                    0x0EE => {
-                        self.pc = self.stack.pop().unwrap();
-                    }
-
-                    // 0nnn - SYS addr
-                    _ => {}
-                }
+            // 00EE - RET
+            Opcode::Return => {
+                self.pc = self.stack.pop().unwrap();
             }
 
             // 1nnn - JP addr
-            0x1 => {
-                self.pc = addr;
+            Opcode::Jump(nnn) => {
+                self.pc = nnn;
             }
 
             // 2nnn - CALL addr
-            0x2 => {
+            Opcode::Call(nnn) => {
                 self.stack.push(self.pc);
-                self.pc = addr;
+                self.pc = nnn;
             }
 
             // 3xkk - SE Vx, byte
-            0x3 => {
-                if self.reg[Vx] == byte {
+            Opcode::SkipEqualByte(x, nn) => {
+                if self.reg[x] == nn {
                     self.pc += 2
                 };
             }
 
             // 4xkk - SNE Vx, byte
-            0x4 => {
-                if self.reg[Vx] != byte {
+            Opcode::SkipNotEqualByte(x, nn) => {
+                if self.reg[x] != nn {
                     self.pc += 2
                 };
             }
 
             // 5xy0 - SE Vx, Vy
-            0x5 => {
-                if self.reg[Vx] == self.reg[Vy] {
+            Opcode::SkipEqual(x, y) => {
+                if self.reg[x] == self.reg[y] {
                     self.pc += 2
                 };
             }
 
             // 6xkk - LD Vx, byte
-            0x6 => {
-                self.reg[Vx] = byte;
+            Opcode::LoadByte(x, nn) => {
+                self.reg[x] = nn;
             }
 
             // 7xkk - ADD Vx, byte
-            0x7 => {
-                self.reg[Vx] = self.reg[Vx].wrapping_add(byte);
+            Opcode::AddByte(x, nn) => {
+                self.reg[x] = self.reg[x].wrapping_add(nn);
             }
 
-            0x8 => {
-                match n {
-                    // 8xy0 - LD Vx, Vy
-                    0x0 => {
-                        self.reg[Vx] = self.reg[Vy];
-                    }
+            // 8xy0 - LD Vx, Vy
+            Opcode::Load(x, y) => {
+                self.reg[x] = self.reg[y];
+            }
 
-                    // 8xy1 - OR Vx, Vy
-                    0x1 => {
-                        self.reg[Vx] |= self.reg[Vy];
-                    }
+            // 8xy1 - OR Vx, Vy
+            Opcode::Or(x, y) => {
+                self.reg[x] |= self.reg[y];
+            }
 
-                    // 8xy2 - AND Vx, Vy
-                    0x2 => {
-                        self.reg[Vx] &= self.reg[Vy];
-                    }
+            // 8xy2 - AND Vx, Vy
+            Opcode::And(x, y) => {
+                self.reg[x] &= self.reg[y];
+            }
 
-                    // 8xy3 - XOR Vx, Vy
-                    0x3 => {
-                        self.reg[Vx] ^= self.reg[Vy];
-                    }
+            // 8xy3 - XOR Vx, Vy
+            Opcode::Xor(x, y) => {
+                self.reg[x] ^= self.reg[y];
+            }
 
-                    // 8xy4 - ADD Vx, Vy
-                    0x4 => {
-                        let (res, carry) = self.reg[Vx].overflowing_add(self.reg[Vy]);
+            // 8xy4 - ADD Vx, Vy
+            Opcode::Add(x, y) => {
+                let (res, carry) = self.reg[x].overflowing_add(self.reg[y]);
 
-                        self.reg[Vx] = res;
-                        self.reg[0xF] = carry as u8;
-                    }
+                self.reg[x] = res;
+                self.reg[0xF] = carry as u8;
+            }
 
-                    // 8xy5 - SUB Vx, Vy
-                    0x5 => {
-                        let (res, borrow) = self.reg[Vx].overflowing_sub(self.reg[Vy]);
-                        self.reg[Vx] = res;
-                        self.reg[0xF] = !borrow as u8;
-                    }
+            // 8xy5 - SUB Vx, Vy
+            Opcode::Sub(x, y) => {
+                let (res, borrow) = self.reg[x].overflowing_sub(self.reg[y]);
+                self.reg[x] = res;
+                self.reg[0xF] = !borrow as u8;
+            }
 
-                    // 8xy6 - SHR Vx {, Vy}
-                    0x6 => {
-                        self.reg[0xF] = self.reg[Vx] & 1;
-                        self.reg[Vx] >>= 1;
-                    }
+            // 8xy6 - SHR Vx {, Vy}
+            Opcode::ShiftRight(x) => {
+                self.reg[0xF] = self.reg[x] & 1;
+                self.reg[x] >>= 1;
+            }
 
-                    // 8xy7 - SUBN Vx, Vy
-                    0x7 => {
-                        let (res, borrow) = self.reg[Vy].overflowing_sub(self.reg[Vx]);
-                        self.reg[Vx] = res;
-                        self.reg[0xF] = !borrow as u8;
-                    }
+            // 8xy7 - SUBN Vx, Vy
+            Opcode::SubN(x, y) => {
+                let (res, borrow) = self.reg[y].overflowing_sub(self.reg[x]);
+                self.reg[x] = res;
+                self.reg[0xF] = !borrow as u8;
+            }
 
-                    // 8xyE - SHL Vx {, Vy}
-                    0xE => {
-                        self.reg[0xF] = (self.reg[Vx] >> 7) & 1;
-                        self.reg[Vx] <<= 1;
-                    }
-
-                    _ => {
-                        panic!("Invalid instruction: {:?}", op);
-                    }
-                }
+            // 8xyE - SHL Vx {, Vy}
+            Opcode::ShiftLeft(x) => {
+                self.reg[0xF] = (self.reg[x] >> 7) & 1;
+                self.reg[x] <<= 1;
             }
 
             // 9xy0 - SNE Vx, Vy
-            0x9 => {
-                if self.reg[Vx] != self.reg[Vy] {
+            Opcode::SkipNotEqual(x, y) => {
+                if self.reg[x] != self.reg[y] {
                     self.pc += 2
                 };
             }
 
             // Annn - LD I, addr
-            0xA => {
-                self.i = addr;
+            Opcode::LoadI(nnn) => {
+                self.i = nnn;
             }
 
             // Bnnn - JP V0, addr
-            0xB => {
-                self.pc = (self.reg[0x0] as u16) + addr;
+            Opcode::JumpV0(nnn) => {
+                self.pc = (self.reg[0x0] as u16) + nnn;
             }
 
             // Cxkk - RND Vx, byte
-            0xC => {
-                self.reg[Vx] = (self.rng)() & byte;
+            Opcode::Random(x, nn) => {
+                self.reg[x] = (self.rng)() & nn;
             }
 
             // Dxyn - DRW Vx, Vy, nibble
-            0xD => {
-                let x = self.reg[Vx] as u16;
-                let y = self.reg[Vy] as u16;
-                let height = n;
+            Opcode::Draw(x, y, n) => {
+                let x = self.reg[x] as u16;
+                let y = self.reg[y] as u16;
+                let height = n as u16;
 
                 self.reg[0xF] = 0;
 
@@ -288,103 +384,86 @@ impl Chip8 {
                 }
             }
 
-            0xE => {
-                match byte {
-                    // Ex9E - SKP Vx
-                    0x9E => {
-                        let key = self.reg[Vx] as usize;
-                        if self.keypad[key] {
-                            self.pc += 2
-                        };
-                    }
+            // Ex9E - SKP Vx
+            Opcode::SkipKeyPress(x) => {
+                let key = self.reg[x] as usize;
+                if self.keypad[key] {
+                    self.pc += 2
+                };
+            }
 
-                    // ExA1 - SKNP Vx
-                    0xA1 => {
-                        let key = self.reg[Vx] as usize;
-                        if !self.keypad[key] {
-                            self.pc += 2
-                        };
-                    }
+            // ExA1 - SKNP Vx
+            Opcode::SkipKeyNotPress(x) => {
+                let key = self.reg[x] as usize;
+                if !self.keypad[key] {
+                    self.pc += 2
+                };
+            }
 
-                    _ => {
-                        panic!("Invalid instruction: {:#04X}", op);
-                    }
+            // Fx07 - LD Vx, DT
+            Opcode::LoadDelayTimer(x) => {
+                self.reg[x] = self.dt;
+            }
+
+            // Fx0A - LD Vx, K
+            Opcode::LoadKeyPress(x) => {
+                if let Some(i) = self.keypad.iter().position(|&key| key) {
+                    self.reg[x] = i as u8;
+                } else {
+                    self.pc -= 2;
                 }
             }
 
-            0xF => {
-                match byte {
-                    // Fx07 - LD Vx, DT
-                    0x07 => {
-                        self.reg[Vx] = self.dt;
-                    }
+            // Fx15 - LD DT, Vx
+            Opcode::LoadDelayTimerSet(x) => {
+                self.dt = self.reg[x];
+            }
 
-                    // Fx0A - LD Vx, K
-                    0x0A => {
-                        for i in 0..16 as u8 {
-                            if self.keypad[i as usize] {
-                                self.reg[Vx] = i;
-                                return;
-                            }
-                        }
+            // Fx18 - LD ST, Vx
+            Opcode::LoadSoundTimer(x) => {
+                self.st = self.reg[x];
+            }
 
-                        self.pc -= 2;
-                    }
+            // Fx1E - ADD I, Vx
+            Opcode::AddI(x) => {
+                self.i += self.reg[x] as u16;
+            }
 
-                    // Fx15 - LD DT, Vx
-                    0x15 => {
-                        self.dt = self.reg[Vx];
-                    }
+            // Fx29 - LD F, Vx
+            Opcode::LoadFont(x) => {
+                let digit = self.reg[x];
 
-                    // Fx18 - LD ST, Vx
-                    0x18 => {
-                        self.st = self.reg[Vx];
-                    }
+                self.i = FONTSET_START_ADDRESS as u16 + digit as u16 * 5;
+            }
 
-                    // Fx1E - ADD I, Vx
-                    0x1E => {
-                        self.i += self.reg[Vx] as u16;
-                    }
+            // Fx33 - LD B, Vx
+            Opcode::LoadBCD(x) => {
+                let mut value = self.reg[x];
 
-                    // Fx29 - LD F, Vx
-                    0x29 => {
-                        let digit = self.reg[Vx];
+                self.mem[(self.i + 2) as usize] = value % 10;
+                value /= 10;
+                self.mem[(self.i + 1) as usize] = value % 10;
+                value /= 10;
+                self.mem[self.i as usize] = value % 10;
+            }
 
-                        self.i = FONTSET_START_ADDRESS as u16 + digit as u16 * 5;
-                    }
-
-                    // Fx33 - LD B, Vx
-                    0x33 => {
-                        let mut value = self.reg[Vx];
-
-                        self.mem[(self.i + 2) as usize] = value % 10;
-                        value /= 10;
-                        self.mem[(self.i + 1) as usize] = value % 10;
-                        value /= 10;
-                        self.mem[self.i as usize] = value % 10;
-                    }
-
-                    // Fx55 - LD [I], Vx
-                    0x55 => {
-                        for v in 0..=Vx {
-                            self.mem[self.i as usize + v] = self.reg[v];
-                        }
-                    }
-
-                    // Fx65 - LD Vx, [I]
-                    0x65 => {
-                        for v in 0..=Vx {
-                            self.reg[v] = self.mem[self.i as usize + v];
-                        }
-                    }
-
-                    _ => {
-                        panic!("Invalid instruction: {:#04X}", op);
-                    }
+            // Fx55 - LD [I], Vx
+            Opcode::StoreRegisters(x) => {
+                for v in 0..=x {
+                    self.mem[self.i as usize + v] = self.reg[v];
                 }
             }
-            _ => {
-                panic!("Invalid instruction: {:#04X}", op);
+
+            // Fx65 - LD Vx, [I]
+            Opcode::LoadRegisters(x) => {
+                for v in 0..=x {
+                    self.reg[v] = self.mem[self.i as usize + v];
+                }
+            }
+
+            // Invalid opcode
+            Opcode::Invalid(_) => {
+                return Err(Chip8Error::InvalidInstruction(raw_opcode));
             }
         }
 
@@ -394,5 +473,7 @@ impl Chip8 {
         if self.st > 0 {
             self.st -= 1;
         }
+
+        return Ok(());
     }
 }
